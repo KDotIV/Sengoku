4eb950f091e9d4d4c08965bd456a0e318e7b2b39
KDotIV <kylebsmall32@gmail.com>
2021-06-22 15:04:34 -0400

    Redo w/ GraphQL

--- front-end/src/pages/Error.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Error = () => {
-    return (
-        <div>
-            hello from Error
-        </div>
-    )
-}
--- front-end/src/pages/Organizers.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Organizers() {
-    return (
-        <div>
-            Hi From Organizers
-        </div>
-    )
-}
